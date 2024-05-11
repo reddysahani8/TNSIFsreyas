eb1efcfee5e846938e2075eea14c470764371bb0
reddysahani8 <130537983+reddysahani8@users.noreply.github.com>
2024-05-11 14:30:02 +0530

    Add files via upload

---
+++ CustomerEntity/CustomerEntity/src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -0,0 +1,52 @@
+package com.dailycodebuffer.Springboot.tutorial.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Department {
+	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+	private String name;
+	private String email;
+	public Department() {
+		
+	}
+	public Department(Long id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+	
+	
+	
+}
---
+++ CustomerEntity/CustomerEntity/src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -0,0 +1,63 @@
+package com.dailycodebuffer.Springboot.tutorial.service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+//import com.dailycodebuffer.Springboot.tutorial.error.DepartmentNotFoundException;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
+   @Override
+   public Department fetchDepartmentById(Long departmentId) {
+	   return departmentRepository.findById(departmentId).get();
+   }
+	
+   @Override
+   public void deleteDepartmentById(Long departmentId) {
+       departmentRepository.deleteById(departmentId);
+   }
+
+
+   @Override
+   public Department updateDepartment(Long departmentId, Department department) {
+       Department depDB = departmentRepository.findById(departmentId).get();
+
+       if(Objects.nonNull(department.getName()) &&
+       !"".equalsIgnoreCase(department.getName())) {
+           depDB.setName(null);
+       }
+
+       
+
+       if(Objects.nonNull(department.getEmail()) &&
+               !"".equalsIgnoreCase(department.getEmail())) {
+           depDB.setEmail(null);
+       }
+
+       return departmentRepository.save(depDB);
+   }
+
+    
+    
+
+}
